5f6bbe4b0d8fc9e44da2539b038017890de7a650
vibedawg <lolkekakniet@mail.ru>
2023-12-12 00:36:41 +0600

    js h.w

---
+++ variables/variable.js
@@ -0,0 +1,44 @@
+// console.log("Hello my name is Akniet.I'm 16 years old")
+
+// let f_name="KIM"
+// let l_name="KARDASHIAN"
+// let age = 32
+// let city="Almaty"
+// let county =`KZ`
+// console.log(age, f_name, l_name, age, city, county)
+// console.log(`my name is ${f_name} ${l_name}.I'm ${age} years old.I live in ${city}`)
+
+// let a = 5
+// let b = 6
+// console.log(`${a} * ${b} =`,a*b)
+// let c = 4526
+// let d = 237
+// let e = 485
+// let f = 123
+
+// console.log(`${c} * ${d} + ${e} * ${f}=`,c*d+e*f)
+// console.log(`${4526}*${237}+${485}*${123}=${4526*237+485*123}`)
+
+// const name="Akniet"
+
+// 1 ex
+let fav_color='blue'
+let fav_song='Glimpse of us'
+let fav_movie='1+1'
+let fav_animal='cats'
+console.log(`All people love something, and this is individual for everyone. The world around us is full of love. I would like to tell you about my favorite things. I like to be alone with myself and listen to music, it gives me more energy and inspiration. My favorite music is Joji – ${fav_song} I also like to watch different films, and my favorite is ${fav_movie}. I also want to add that I adore ${fav_animal}. ${fav_animal} are the cutest and most affectionate creatures on this planet. I like to give ${fav_animal} my love because it is reciprocated. I also like the ${fav_color} light; I associate this color with the summer sky and the sea with waves. That's why I like it because I was born in the summer. By this I wanted to say that the world is full of love and beauty.`)
+
+//2 ex
+let hascoffee= true
+console.log(`у вас ${hascoffee ? 'есть' : 'нет'} кофе`)
+
+//3
+let isweekend=false
+console.log(`Сегодня ${isweekend ? 'выходной' : 'нет выходного'} `)
+
+//4
+
+//5
+let history =`Today the weather is very cool, the long-awaited snow is falling, and it’s -8 degrees Celsius outside, and Fahrenheit:`
+let Fahrenheit=(-8*9/5)+32
+console.log(`${history}`,`${Fahrenheit} degrees`)
