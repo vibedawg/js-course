8284f5ac6c70f12145e6a2ebfe5d7e8088ef0af5
vibedawg <lolkekakniet@mail.ru>
2023-12-14 09:44:44 +0600

    js 14.12 hw

---
+++ data types/data.js
@@ -0,0 +1,35 @@
+// console.log(Math.round(7.87)*72)
+// console.log(Math.floor(4.99))
+// console.log(Math.ceil(7.1))
+// console.log(Math.ceil(Math.max(-5, 3, 20, 4, 5, 10) / 787))
+
+// let a = (Math.ceil(Math.max(-5, 3, 20, 4, 5, 10)/ 999))
+// let b = (Math.floor(Math.min(-5, 3, 20, 4, 5, 10) / 111))
+// console.log(a + b)
+
+
+// let num1 = Math.floor(Math.random () * 71) 
+// let num2 = Math.floor(Math.random () * 81)
+// let num3 = Math.floor(Math.random () * 91)
+// let num4 = Math.floor((num1+num2)/num3)
+// console.log(`${num1}+${num2} / ${num3} = ${num4}`)
+
+
+//hw 
+//ex 1 
+console.log(Math.min(14,32,9,27,41))
+//ex 2
+console.log(Math.ceil(Math.min(14,32,9,27,41) / 745))
+//ex 3 
+console.log(Math.ceil(Math.random () * 50))
+//ex 4
+let ex1 = Math.round(Math.min(14,32,9,27,41) / 745)
+let ex2 = Math.ceil(Math.random () * 50)
+console.log(ex1 + ex2)
+//level 2
+//ex 7
+console.log(Math.ceil(Math.random () * 100))
+//ex 8
+// console.log(Math.ceil(Math.random() * 100)) + 50   ???
+//ex 9 
+console.log(Math.ceil(Math.random () * 255))
