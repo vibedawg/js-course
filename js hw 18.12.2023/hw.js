57c8cb4c6ef003d53554a8de604154bd558fb786
vibedawg <lolkekakniet@mail.ru>
2023-12-18 14:44:48 +0600

    18.12.23 hw

---
+++ js hw 18.12.2023/hw.js
@@ -0,0 +1,75 @@
+//lvl 1 
+//ex 1 
+let challenge='30 Days of JavaScript'
+//ex 2
+console.log(challenge)
+//ex 3
+console.log(challenge.length)
+//ex 4
+console.log(challenge.toUpperCase())
+//ex 5
+console.log(challenge.toLowerCase())
+//ex 6
+console.log(challenge.substr(3))
+//ex 7
+console.log(challenge.substr(0,3))
+//ex 8
+console.log(challenge.includes('Script'))
+//ex 9
+console.log(challenge.split()) 
+//ex 10
+console.log(challenge.split(" ")) 
+//ex 11
+let language = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+console.log(language.split(', '))
+//ex 12
+rename = challenge.replace("JavaScript" , "Python" )
+console.log(rename)
+//ex 13
+console.log(challenge.charAt(15))
+//ex 14
+console.log(challenge.charCodeAt(11))
+//ex 15
+console.log(challenge.indexOf('a'))
+//ex 16
+console.log(challenge.lastIndexOf('30 Days of JavaScript'))
+//ex 17
+let because = 'You cannot end a sentence with because because because is a conjunction'
+console.log(because.indexOf('because'))
+//ex 18
+console.log(because.lastIndexOf('because'))
+//ex 19
+console.log(because.search('because'))
+//ex 20
+let string = ' 30 Days of JavaScript '
+console.log(string.trim(' '))
+//ex 21
+console.log(challenge.startsWith('30'))
+//ex 22
+console.log(challenge.endsWith('ipt'))
+//ex 23
+console.log(challenge.match('a'))
+//ex 24
+let days = '30 Days of '
+console.log(days.concat('JavaScript'))
+//ex 25
+console.log(challenge.repeat(2))
+
+//lvl 2 
+//ex 1 
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.")
+//ex 2
+console.log(`"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."`)
+//ex 7
+console.log(Math.ceil(Math.random () * 100))
+//ex 8
+console.log(Math.ceil(Math.random() * 50)+ 50) 
+//ex 9 
+console.log(Math.ceil(Math.random () * 255))
+//ex 12
+console.log(because.substr(because.indexOf('because')))  //???
+
+//lvl 3
+//ex 1
+let love='Love is the best thing in this world. Some found their love and some are still looking for their love.'
+console.log(love.search('love'))  //???
